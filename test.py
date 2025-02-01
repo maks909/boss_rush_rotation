381ae1d9700b8e1e3425cd446acd411cf2e24de5
Maxim <kuzalkovmaxim@gmail.com>
2025-02-01 14:43:25 +0100

    Changed the bacckground color and tried to repair the animations.

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ window.collider_counter.enabled = False
 window.exit_button.enabled = True
 window.cog_button.enabled=False
 window.always_on_top=True
-window.color=color.white
+window.color=color.black
 
 class Player(Entity):
     def __init__(self):
@@ -25,10 +25,10 @@ class Player(Entity):
         self.collider="box"
         
         self.start_textures={"left": (load_texture(f'assets/start_left{i:03d}.gif') for i in range(1, 5)), "right": (load_texture(f'assets/start_right{i:03d}.gif') for i in range(1, 5))}
-
+        
         self.run_textures={"left": (load_texture(f'assets/left_running{i:03d}.gif') for i in range(1, 4)), "right": (load_texture(f'assets/right_running{i:03d}.gif') for i in range(1, 4))}
-        self.texture=self.start_textures["right"][0]
-
+        self.texture=self.start_textures["right"]
+        self.model="quad"
         self.current_textures=self.run_textures
 
 
@@ -38,12 +38,13 @@ class Player(Entity):
         if held_keys["down arrow"]: 
             self.y-=self.speed*time.dt
         if not (held_keys["right arrow"] or held_keys["left arrow"] or held_keys["up arrow"] or held_keys["down arrow"]):
-            self.current_textures = self.start
+            pass
+            #self.current_textures = self.start
         if held_keys["right arrow"]:  
-            self.current_textures = self.run_textures["right"]
+            #self.current_textures = self.run_textures["right"]
             self.x+=self.speed*time.dt
         if held_keys["left arrow"]:   
-            self.current_textures = self.run_textures["left"]
+            #self.current_textures = self.run_textures["left"]
             self.x-=self.speed*time.dt    
 
 player=Player()
